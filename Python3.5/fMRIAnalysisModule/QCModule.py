c156534e9fff4937cc56a3ad71feee4f5fd532d8
weidongcai <weidongcai.us@gmail.com>
2017-01-19 17:23:22 -0800

    update QC scripts

--- Python3.5/fMRIAnalysisModule/QCModule.py
+++ Python3.5/fMRIAnalysisModule/QCModule.py
@@ -13,8 +13,19 @@ import nilearn.image
 from nilearn import plotting
 import matplotlib.pyplot as plt
 
+class StatsCoV(object):
+  """define a class for coefficient of variation statisticas
+  """
+  def __init__(self, mean=0, std=0):
+    """ initialize class variables 'mean' and 'std'
+    """
+    self.mean = mean
+    self.std = std  
+
+
 def CoefficientOfVariation4D(inputFilename, maskFilename):
-### compute voxelwise coefficient of variation on 4D fMRI data ###
+  """ compute voxelwise coefficient of variation on 4D fMRI data
+  """
   img = nib.load(inputFilename)
   img_data = img.get_data()
   img_mask = nib.load(maskFilename)
@@ -23,12 +34,16 @@ def CoefficientOfVariation4D(inputFilename, maskFilename):
   mean_img = np.zeros(img_mask_data.shape)
   img_data_nanmean = np.nanmean(img_data, axis=3)
   img_data_nanstd = np.nanstd(img_data, axis=3)
-  print(img.affine)
-  print(img.header)
-  #img_nanmean = nib.Nifti1Image(img_data_nanmean, img_mask.affine, img_mask.header)
-  #img_nanmean.to_filename('test_nanmean1.nii.gz')
-  img_nanstd = nib.Nifti1Image(img_data_nanstd, np.eye(4), img.affine)
-  img_nanstd.to_filename('test_nanstd1.nii.gz')
-  stats = [1,2,3]
+  img_data_cv = np.divide(img_data_nanstd, img_data_nanmean)
+  print(img_data_cv.shape)
+
+  ### if need to save nanmean or nanstd images ###
+  #img_nanmean = nib.Nifti1Image(img_data_nanmean, img.affine, img.header)
+  #img_nanmean.to_filename('test_nanmean.nii.gz')
+  #img_nanstd = nib.Nifti1Image(img_data_nanstd, img.affine, img.header)
+  #img_nanstd.to_filename('test_nanstd.nii.gz')
+  ###
+
+  stats = StatsCoV(np.nanmean(img_data_cv), np.nanstd(img_data_cv))
   return(stats)
   

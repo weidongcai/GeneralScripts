9d950967956a97adefbb5b4c4f1737c133c4dc35
weidongcai <weidongcai.us@gmail.com>
2017-01-22 17:49:59 -0800

    update StatsModule

--- Python3.5/CommonModule/StatsModule.py
+++ Python3.5/CommonModule/StatsModule.py
@@ -7,6 +7,22 @@ import numpy as np
 import statsmodels
 
 def Regression(y, X):
-  moorePenrosePseduoInverseX = np.dot(np.linalg.inv(np.dot(np.transpose(X), X)), np.transpose(X))
-  b = np.dot(moorePenrosePseduoInverseX, y)
-  return b
+  '''
+  multiple linear regression:
+  y is a nxk matrix: n is observation sample size, k is observation channel size
+  X is a nxp matrix: p is number of regressors
+  note: check constants are added in the X
+  '''
+  # check whether constants are added in the X
+  if not (np.sum(X[:,0])==X.shape[0]):
+    print('Error: constants are not added in the regression')
+    sys.exit(1)
+  else:
+    moorePenrosePseduoInverseX = np.dot(np.linalg.inv(np.dot(np.transpose(X), X)), np.transpose(X))
+    b = np.dot(moorePenrosePseduoInverseX, y)
+    return b
+
+def ResidualFromRegression(y, X):
+  b = Regression(y, X):
+  residual = y - np.dot(X, b)
+  return residual
